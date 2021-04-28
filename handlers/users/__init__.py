0b8b7a72f5579a51c8c7aa3ae4a1e9a8cfaf7c50
Mark <marcus.koh16.01.97@gmail.com>
2021-04-29 00:00:00 +0300

    upd

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -3,9 +3,6 @@ from .purchase import dp
 from .work_menu import dp
 from .testing import dp
 from .start import dp
-from .encode import dp
-from .decode import dp
-from .add import dp
 from .admin import dp
 
 __all__ = ["dp"]
